756653ee673de0ffdfd2a15101f640fab5ce1489
grandpoisson <grandpoisson@gmail.com>
2021-01-09 23:59:27 +0000

    add new login files

--- functions.php
+++ functions.php
@@ -2,6 +2,13 @@
 <?php
 
 function login(){
+//Initialize session
+session_start();
+
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
 
 
 }
