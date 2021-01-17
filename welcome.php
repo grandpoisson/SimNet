417eb5d5cb33e3169857198cdb86b9f4b6c2abca
grandpoisson <grandpoisson@gmail.com>
2021-01-17 05:35:55 +0000

    corrected path to css

--- welcome.php
+++ welcome.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <meta charset="UTF-8">
 <title>User Login</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-<link href="./view/css/style.css" rel="stylesheet" type="text/css" />
+<link href="./css/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     <div>
